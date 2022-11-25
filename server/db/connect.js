b5f3697b5a00f42f43f5f814715e458a262be70b
Kunal Patel <goudanikunal3@gmail.com>
2022-11-25 19:21:14 +0530

    connected to atlas via mongo compass

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Registration',{
+mongoose.connect('mongodb+srv://Kunal123:codinggujjus@users.iuusecl.mongodb.net/test',{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 })
