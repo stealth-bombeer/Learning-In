ef11f13f167b140fd1eb1a791f168ae1385aae18
DEVelooper29 <devbbhuva@gmail.com>
2023-01-19 22:59:04 +0530

    progressbar

--- home/src/Navbar comp/Viewprofile.js
+++ home/src/Navbar comp/Viewprofile.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import {authUser,y} from '../login/Login'
 import {authUserregisterd} from "../login/Register";
+import "./Viewprofile.css"
 
 const Viewprofile = () => {
 
@@ -28,6 +29,14 @@ const Viewprofile = () => {
 
         });
 
+        const [filled, setFilled] = useState(0);
+	const [isRunning, setIsRunning] = useState(false);
+	useEffect(() => {
+		if (filled < 100 && isRunning) {
+			setTimeout(() => setFilled(prev => prev += 2), 50)
+		}
+	},[filled, isRunning])
+
     return (
         <div>
         <p>Hii</p>
@@ -35,8 +44,48 @@ const Viewprofile = () => {
         <p>Hii</p>
         <p>Hii</p>
         <p>Score:{ personalScore }</p>
+
+        <div>
+		  <div className="progressbar">
+			  <div style={{
+				  height: "100%",
+				  width: `${ (personalScore%10)*10}%`,
+				  backgroundColor: "#a66cff",
+				  transition:"width 0.5s"
+			  }}></div>
+			  <span className="progressPercent">Level: {Math.floor(personalScore/10)+1} </span>
+		  </div>
+		  {/* <button className="btn" onClick={() => {setIsRunning(true)}}>Run</button> */}
+	</div>
+
         </div>
       );
 }
  
 export default Viewprofile;
+
+// import React, {useState, useEffect} from 'react'
+
+// export default function Progressbar() {
+// 	const [filled, setFilled] = useState(0);
+// 	const [isRunning, setIsRunning] = useState(false);
+// 	useEffect(() => {
+// 		if (filled < 100 && isRunning) {
+// 			setTimeout(() => setFilled(prev => prev += 2), 50)
+// 		}
+// 	},[filled, isRunning])
+//   return (
+// 	  <div>
+// 		  <div className="progressbar">
+// 			  <div style={{
+// 				  height: "100%",
+// 				  width: `${filled}%`,
+// 				  backgroundColor: "#a66cff",
+// 				  transition:"width 0.5s"
+// 			  }}></div>
+// 			  <span className="progressPercent">{ filled }%</span>
+// 		  </div>
+// 		  <button className="btn" onClick={() => {setIsRunning(true)}}>Run</button>
+// 	</div>
+//   )
+// }
